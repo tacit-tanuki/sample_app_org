07a9dfd7c437edfb1c1717208f3643060f0a12ba
Nobuchika Tanaka <tacit.tanuki@gmail.com>
2024-09-17 20:53:26 +0000

    Revert "演習1の回答"
    This reverts commit 1e25ff2c80823dac0c85f6a337d376e00796e2ee.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,10 @@
 class StaticPagesController < ApplicationController
-  def home; end
+  def home
+  end
 
-  def help; end
+  def help
+  end
 
-  def about; end
-
-  def contact; end
+  def about
+  end
 end
