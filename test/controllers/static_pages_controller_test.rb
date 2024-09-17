ca8fdfcd1baf5c33c68d0019c5f6ba9844dd2a8b
Nobuchika Tanaka <tacit.tanuki@gmail.com>
2024-09-17 21:02:22 +0000

    Revert "演習1の回答"
    This reverts commit 6b3ce94b5b9b2175d30422551abb55e0b86a9321.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,11 +1,6 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test 'should get root' do
-    get root_url
-    assert_response :success
-  end
-
   test 'should get home' do
     get static_pages_home_url
     assert_response :success
