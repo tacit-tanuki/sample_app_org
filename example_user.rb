5e10ea08f34b6f3eb20b6b2743eece8f4a3c752d
Nobuchika Tanaka <tacit.tanuki@gmail.com>
2024-09-19 02:28:56 +0000

    演習2の回答

--- example_user.rb
+++ example_user.rb
@@ -11,6 +11,10 @@ class User
     "#{@first_name} #{@last_name}"
   end
 
+  def alphabetical_name
+    "#{@last_name}, #{@first_name}"
+  end
+
   def formatted_email
     "#{full_name} <#{@email}>"
   end
