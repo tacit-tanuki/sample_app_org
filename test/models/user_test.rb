eb365d71f6f061f9ea6dcd2cec6890f992530c25
Nobuchika Tanaka <tacit.tanuki@gmail.com>
2024-09-21 01:46:55 +0000

    演習3の回答

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.new(name: 'Example User', email: 'user@example.com',
-                     password: 'foobar', password_confirmation: 'foobar')
+                     password: 'foobarxx', password_confirmation: 'foobarxx')
   end
 
   test 'should be valid' do
